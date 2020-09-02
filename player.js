912cc59500e64ebc929b7b159176363ad49c2a2a
jennifer klein <jenniejoyness@gmail.com>
2020-09-03 01:04:41 +0300

    parsing data from json file

--- player.js
+++ player.js
@@ -6,44 +6,64 @@ req.open("GET", url);
 req.onreadystatechange = function(){
 	if(this.readyState == 4 && this.status == 200){
 		 const data = req.responseText;
-		 format(data);
+		 req = format(data);
+		 var jsonObject = convertToJson(req);
+		 var dataObject = jsonObject["data"];
+		 var stepsArray = getSteps(dataObject);
+		 var tiplates = getTiplates(dataObject);
+		 getTips(stepsArray);
+
 
 }
 }
 req.responseType = 'text';
 req.send();
 
-function getTips(jsonObj){
-	var first = jsonObj[0];
-	const myH2 = document.createElement('h2');
-	// myH2.textContent = first.title;
-	myH2.textContent = "jennie";
-	console.log(first.title);
-	document.body.appendChild(myH2);
 
-}
 
 function format(response) {
 	var res = response.split("__5szm2kaj(");
 	var res = res[1];
-	res.substring(0, res.length-2);
-	console.log(res);
+	res = res.substring(0, res.length-1);
+	return res;
 }
 
 
-// function convertToJson(textfile){
-// 	console.log(textfile);
-// 	var json = JSON.parse(textfile);
+function convertToJson(textfile){
+	var jsonObject = JSON.parse(textfile);
+	return jsonObject;
 
-// }
+}
 
+function getSteps(dataObject){
+	var structure = dataObject["structure"];
+	var steps = structure["steps"];
+	return steps;
+}
 
-// const input = document.querySelector('input[type="file]')
-// input.addEventListener('change', function(e){
-// 	console.log(input.files)
-// 	const reader = new FileReader()
-// 	reader.onload = function () {
-// 		var result = reader.result;
-// 	}
-// 	reader.readAsText(input.files[0])
-// },false);
+function getTiplates(dataObject){
+	var tiplates = dataObject["tiplates"];
+	var hoverTip = tiplates["hoverTip"];
+	var tip = tiplates["tip"];
+	return [hoverTip,tip];
+}
+function getTips(stepsArray){
+	var first = stepsArray[3];
+	var action = first["action"];
+	var contents = action["contents"];
+	var content = contents["#content"];
+	var selector = action["selector"]
+	makeTip(content,selector);
+	const myH2 = document.createElement('h2');
+	// myH2.textContent = first.title;
+	myH2.textContent =content;
+	
+	document.body.appendChild(myH2);
+
+}
+
+function makeTip(content,selector){
+	var se = document.querySelector(selector);
+	console.log(se);
+
+}
