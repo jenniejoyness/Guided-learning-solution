fec3adb2231cf6e03014e18f1e173c62f32d7baa
jennifer klein <jenniejoyness@gmail.com>
2020-09-03 20:47:43 +0300

    requesting url response with jsonp

--- player.js
+++ player.js
@@ -1,39 +1,61 @@
 var url = 'https://guidedlearning.oracle.com/player/latest/api/scenario/get/v_IlPvRLRWObwLnV5sTOaw/5szm2kaj/?callback=__5szm2kaj&amp;refresh=true&amp;env=dev&amp;type=startPanel&amp;vars%5Btype%5D=startPanel&amp;sid=none&amp;_=1582203987867';
          
-var req = new XMLHttpRequest();
+
 //Http.open("GET", "https://jsonplaceholder.typicode.com/posts");
-req.open("GET", url);
-req.onreadystatechange = function(){
-	if(this.readyState == 4 && this.status == 200){
-		 const data = req.responseText;
-		 req = format(data);
-		 var jsonObject = convertToJson(req);
-		 var dataObject = jsonObject["data"];
-		 var stepsArray = getSteps(dataObject);
-		 var tiplates = getTiplates(dataObject);
-		 getTips(stepsArray);
 
+//httpGetAsync(url,callback)
 
-}
-}
-req.responseType = 'text';
-req.send();
-
-
-
-function format(response) {
-	var res = response.split("__5szm2kaj(");
-	var res = res[1];
-	res = res.substring(0, res.length-1);
-	return res;
-}
-
-
-function convertToJson(textfile){
-	var jsonObject = JSON.parse(textfile);
-	return jsonObject;
+function httpGetAsync(theUrl, callback)
+{
+    var req = new XMLHttpRequest();
+    req.onreadystatechange = function() { 
+        if (req.readyState == 4 && req.status == 200)
+            callback(req.responseText);
+    }
+    req.open("GET", theUrl, true); // true for asynchronous 
+    req.send(null);
 
 }
+
+function jsonp(url, callback) {
+    var callbackName = 'jsonp_callback';
+    window[callbackName] = function(data) {
+        delete window[callbackName];
+        document.body.removeChild(script);
+        callback(data);
+    };
+
+    var script = document.createElement('script');
+    script.src = url + (url.indexOf('?') >= 0 ? '&' : '?') + 'callback=' + callbackName;
+    document.body.appendChild(script);
+}
+
+function callback(jsonpObject) {
+	var dataObject = jsonpObject["data"];
+	var cssContent = dataObject["css"];
+    var stepsArray = getSteps(dataObject);
+	var tiplates = getTiplates(dataObject);
+	var tipArray = getTips(stepsArray);
+	//makeTip(tipArray[0], tipArray[1], tiplates[0]);
+
+	//css file
+	createCss(cssContent);
+
+}
+
+jsonp(url,callback);
+
+
+
+
+function createCss(cssContent) {
+	var style = document.createElement('style');
+	style.type = 'text/css';
+	style.innerHTML = cssContent;
+	document.body.appendChild(style);
+
+}
+
 
 function getSteps(dataObject){
 	var structure = dataObject["structure"];
@@ -48,22 +70,19 @@ function getTiplates(dataObject){
 	return [hoverTip,tip];
 }
 function getTips(stepsArray){
-	var first = stepsArray[3];
+	var first = stepsArray[1];
 	var action = first["action"];
 	var contents = action["contents"];
 	var content = contents["#content"];
 	var selector = action["selector"]
-	makeTip(content,selector);
-	const myH2 = document.createElement('h2');
-	// myH2.textContent = first.title;
-	myH2.textContent =content;
-	
-	document.body.appendChild(myH2);
+	return [content,selector];
 
 }
 
-function makeTip(content,selector){
+function makeTip(content,selector,tiplates){
 	var se = document.querySelector(selector);
-	console.log(se);
+	const div = document.createElement('div');
+	div.textContent = tiplate;
+	document.se.appendChild(div);
 
 }
